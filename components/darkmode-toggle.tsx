486f8541957d5b3c06565d19e65e43e080c1d078
Arghya Das <arghyadasproject@gmail.com>
2024-01-07 19:24:51 +0530

    persona portfolio default dark mode

--- components/darkmode-toggle.tsx
+++ components/darkmode-toggle.tsx
@@ -15,7 +15,10 @@ export function ModeToggle() {
       variant="outline"
       size="icon"
       className="h-[2rem] w-[2rem] rounded-full"
-      onClick={() => toast("DEVS ONLY USE DARK MODE")}
+      onClick={() => {
+        toast("DEVS ONLY USE DARK MODE");
+        setTheme(theme === "light" ? "dark" : "dark");
+      }}
     >
       <SunIcon className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
