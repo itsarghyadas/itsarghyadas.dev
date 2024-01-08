aa19b05e2cf21cb83d4b585a554bf0d7be084830
Arghya Das <arghyadasproject@gmail.com>
2024-01-08 12:45:42 +0530

    persona portfolio hero section blurry gradient

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import TimeLineSection from "@/components/timeline-section";
 export default function Home() {
   return (
     <section className="home relative bg-neutral-800/[0.075]">
-      <div className="relative mx-auto max-w-2xl overflow-hidden">
-        <div className="absolute left-0 top-5 -z-10 m-auto h-[220px] w-[270px] rounded-b rounded-l-xl rounded-r-md rounded-t-full bg-gradient-to-tr from-cyan-500 via-red-500 to-orange-500 opacity-10 blur-[75px]"></div>
-
-        <div className=" backdrop-blur-2xl">
+      <div className=" mx-auto max-w-2xl overflow-hidden">
+        <div className="relative backdrop-blur-2xl">
+          <div className="absolute left-0 top-5 -z-10 m-auto h-[220px] w-[270px] rounded-b rounded-l-xl rounded-r-md rounded-t-full bg-gradient-to-tr from-cyan-500 via-red-500 to-orange-500 opacity-10 blur-[75px]"></div>
           <main className="h-full w-full border border-t-0 border-dashed border-neutral-300/30 dark:border-neutral-700/30">
             <h1 className=" p-5 font-geistMono text-sm font-[550] leading-[1.55rem] text-neutral-300/70 [word-spacing:-0.7px] md:leading-[1.65rem] md:tracking-normal md:[word-spacing:0px]">
               Hi there, I am{" "}
