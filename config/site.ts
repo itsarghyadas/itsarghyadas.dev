5f077ebccc46c66ff157212bd31db43e8ca11af4
Arghya Das <arghyadasproject@gmail.com>
2024-01-08 10:23:09 +0530

    persona portfolio open graph description changes

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,8 @@ export const siteConfig = {
   url: "https://www.itsarghyadas.dev",
   ogImage:
     "https://res.cloudinary.com/dzzhtacky/image/upload/v1704689013/itsarghyadas-og_nkayvk.png",
-  description: "Personal portfolio of Arghya Das, a full-stack developer.",
+  description:
+    "Hey, I'm Arghya Das (Full-Stack Developer). This is my personal portfolio.",
   links: {
     twitter: "https://twitter.com/itsarghyadas",
     github: "https://github.com/itsarghyadas",
