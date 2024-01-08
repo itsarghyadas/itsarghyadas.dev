ad6c9dd51b90037ec1653f6178bb2ef7a4fc742d
Arghya Das <arghyadasproject@gmail.com>
2024-01-08 12:33:45 +0530

    persona portfolio timeline border top added

--- components/timeline-section.tsx
+++ components/timeline-section.tsx
@@ -41,13 +41,13 @@ const timelineData = [
 export default function TimeLineSection() {
   return (
     <section className="timeline">
-      <div className="mx-auto w-full max-w-2xl border border-b-0 border-t-0 border-dashed border-neutral-300/30 p-5 dark:border-neutral-700/30 md:p-10">
+      <div className="mx-auto w-full max-w-2xl border border-b-0 border-t border-dashed border-neutral-300/30 p-5 dark:border-neutral-700/30 md:p-10">
         <h2 className="pb-5 font-geistMono text-sm font-semibold underline underline-offset-4">
           LIFE TIMELINE
         </h2>
-        <ol className="relative border-s border-dashed border-gray-200 dark:border-neutral-800">
+        <ol className="timeline__list relative border-s border-dashed border-gray-200 dark:border-neutral-800">
           {timelineData.map((event) => (
-            <li key={event.id} className="mb-10 ms-4">
+            <li key={event.id} className="timeline__list__item mb-10 ms-4">
               <div className="absolute -start-[7px] mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700">
                 <span className="relative flex h-3 w-3">
                   <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-cyan-400/50 opacity-75"></span>
@@ -62,7 +62,7 @@ export default function TimeLineSection() {
               >
                 {event.title}
               </h3>
-              <p className="mb-4 mt-1.5 font-geistMono text-xs font-semibold tracking-tighter text-neutral-400/80 md:text-sm">
+              <p className="event__description mb-4 mt-1.5 font-geistMono text-xs font-semibold tracking-tighter text-neutral-400/80 md:text-sm">
                 {event.description}
               </p>
             </li>
